3beb577057dac1bf69729c2212b6dff50bb4dc22
Bruno Santos <brunoasantos2@gmail.com>
2019-12-17 13:29:49 -0300

    Filter out fields when creating audit

--- app/models/concerns/auditable.rb
+++ app/models/concerns/auditable.rb
@@ -1,6 +1,8 @@
 module Auditable
   extend ActiveSupport::Concern
 
+  SKIP_FIELDS = %w[updated_at created_at].freeze
+
   included do
     has_many :audits, as: :auditable
 
@@ -20,11 +22,27 @@ module Auditable
   private
 
   def create_audit(entity, type)
-    Audit.create(
+    return unless should_audit?(entity, type)
+
+    Audit.create!(
       user: User.first,
       auditable: entity,
-      audit_changes: entity.saved_changes,
+      audit_changes: get_auditable_changes(entity, type),
       audit_type: type
     )
+  end
+
+  def should_audit?(entity, type)
+    type == :destroy || auditable_fields(entity).present?
+  end
+
+  def get_auditable_changes(entity, type)
+    return {} unless type == :update
+
+    entity.saved_changes.slice(*auditable_fields(entity))
+  end
+
+  def auditable_fields(entity)
+    @_auditable_fields ||= entity.saved_changes.keys - SKIP_FIELDS
   end
 end
--- test/models/audit_test.rb
+++ test/models/audit_test.rb
@@ -3,20 +3,76 @@ require 'test_helper'
 class AuditTest < ActiveSupport::TestCase
   setup do
     @user = users(:one)
+    @election = Election.new(name: 'Test', start_at: Time.now, end_at: Time.now, user: @user, settings: { visibility: :public })
   end
 
-  test 'creates audit for election' do
-    election = Election.new(name: 'Test', start_at: Time.now, end_at: Time.now, user: @user, settings: { visibility: :public })
+  test 'creates audit when creating an entity' do
     assert_difference('Audit.count') do
-      election.save
+      @election.save
     end
 
     audit = Audit.last
 
     assert_equal 'Election', audit.auditable_type
-    assert_equal election.id, audit.auditable_id
+    assert_equal @election.id, audit.auditable_id
     assert_equal @user.id, audit.user_id
-    assert audit.audit_changes
     assert_equal 'create', audit.audit_type
+    assert_equal({}, audit.audit_changes)
+  end
+
+  test 'creates audit when updating an entity' do
+    @election.save
+    @election.name = 'Changed'
+
+    assert_difference('Audit.count') do
+      @election.save
+    end
+
+    audit = Audit.last
+
+    assert_equal 'Election', audit.auditable_type
+    assert_equal @election.id, audit.auditable_id
+    assert_equal @user.id, audit.user_id
+    assert_equal 'update', audit.audit_type
+    assert_equal({'name' => ['Test', 'Changed']}, audit.audit_changes)
+  end
+
+  test 'creates audit when deleting an entity' do
+    @election.save
+
+    assert_difference('Audit.count') do
+      @election.destroy
+    end
+
+    audit = Audit.last
+
+    assert_equal 'Election', audit.auditable_type
+    assert_equal @election.id, audit.auditable_id
+    assert_equal @user.id, audit.user_id
+    assert_equal 'destroy', audit.audit_type
+    assert_equal({}, audit.audit_changes)
+  end
+
+
+  test 'only create audit if something changed' do
+    @election.save
+
+    assert_difference('Audit.count', 0) do
+      @election.touch
+    end
+  end
+
+  test 'skip created_at and updated_at on audits' do
+    @election.save
+    @election.name = 'Changed'
+    @election.save
+
+    audit = Audit.last
+
+    assert_equal 'Election', audit.auditable_type
+    assert_equal @election.id, audit.auditable_id
+    assert_equal @user.id, audit.user_id
+    assert_equal({'name' => ['Test', 'Changed']}, audit.audit_changes)
+    assert_equal 'update', audit.audit_type
   end
 end
