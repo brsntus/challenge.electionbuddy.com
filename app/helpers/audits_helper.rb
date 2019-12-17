d495704daf873026a0c078d427f3b9700795536f
Bruno Santos <brunoasantos2@gmail.com>
2019-12-17 13:50:45 -0300

    Add helpers and partial for rendering audit changes

--- app/helpers/audits_helper.rb
+++ app/helpers/audits_helper.rb
@@ -1,2 +1,16 @@
 module AuditsHelper
+  def auditable_name(audit)
+    "#{audit.auditable_type} ##{audit.auditable_id}"
+  end
+
+  def audit_changes(audit)
+    case audit.audit_type
+    when 'create'
+      'Created'
+    when 'destroy'
+      'Destroy'
+    when 'update'
+      render partial: 'audit_changes', locals: { changes: audit.audit_changes }
+    end
+  end
 end
